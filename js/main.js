9ffe4d47de351e3f2d8e1f1773c0e6cec21a53da
Massimiliano Ambrogio <maxi.ambrogio@gmail.com>
2020-11-10 18:01:31 +0100

    Project 33 parte 1.0 add and fix html css js

---
+++ js/main.js
@@ -0,0 +1,17 @@
+/**
+ * Creare una Todolist con Vue.js dove l'utente 
+ * ha la possibilità di:
+   Creare un nuovo todo attraverso un campo di testo
+   Rimuovere uno specifico todo attraverso 
+   il click su un icona per la rimozione nel todo stesso.
+   Dare un feedback all'utente nell'interfaccia se 
+   non ci sono elementi todo nella lista.
+ */
+
+// Parte Vue
+const app = new Vue({
+    el: '#app',
+        data: {
+             logo: './img/logo.png'
+    },
+});
